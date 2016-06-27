6a6195ff66e14e30bd5fa77d88f97c3159805ca0
abikadabra <abidarda28@gmail.com>
2016-06-27 23:01:10 +0700

    Chapter5 example14

---
+++ src/chapter5/Eg5_14_Palindrome.java
@@ -0,0 +1,33 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/**
+ * Created by USER on 6/27/2016.
+ */
+public class Eg5_14_Palindrome {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+        int low = 0;
+        int high = s.length()-1;
+
+        boolean isPalindrome = true;
+        while (low<high){
+            if (s.charAt(low) !=s.charAt(high)){
+                isPalindrome = false;
+                break;
+            }
+
+            low++;
+            high--;
+        }
+        if (isPalindrome){
+            System.out.print(s+" is a palindrome");
+        }else {
+            System.out.println(s+" is not a palindrome");
+        }
+    }
+}
