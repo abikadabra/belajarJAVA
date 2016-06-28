4bdbf2714b1ef44d34dd3e5e316a40a9c4bf13f4
abikadabra <abidarda28@gmail.com>
2016-06-28 21:31:15 +0700

    Chapter5 exercise15

---
+++ src/chapter5/Ex5_15_DisplayTheASCIICharacterTable.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+/**
+ * Created by USER on 6/28/2016.
+ */
+public class Ex5_15_DisplayTheASCIICharacterTable {
+    public static void main(String[]args){
+        for (char i = '!';i<='~';i++){
+            System.out.println(i);
+
+        }
+    }
+}
