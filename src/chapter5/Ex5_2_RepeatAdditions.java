8f085d3b73b8e81f246a42102d7b5f90b3845eb5
abikadabra <abidarda28@gmail.com>
2016-06-28 10:26:43 +0700

    Chapter5 exercise2

---
+++ src/chapter5/Ex5_2_RepeatAdditions.java
@@ -0,0 +1,50 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/**
+ * Created by USER on 6/28/2016.
+ */
+public class Ex5_2_RepeatAdditions {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+
+        final int NUMBER_OF_QUESTIONS = 5;
+        int correctCount = 0;
+        int count = 0;
+        long startTime = System.currentTimeMillis();
+        String output = " ";
+
+        while (count < NUMBER_OF_QUESTIONS) {
+            int number1 = (int) (Math.random() * 16);
+            int number2 = (int) (Math.random() * 16);
+
+            if (number1 < number2) {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            System.out.print("What is " + number1 + " - " + number2 + "? ");
+            int answer = input.nextInt();
+
+            if (number1 - number2 == answer) {
+                System.out.println("You are correct!");
+                correctCount++;
+            } else {
+                System.out.print("Your answer is wrong." + number1 + " - " + number2 + " should be " + (number1 - number2));
+
+                count++;
+
+                output += "\n" + number1 + "-" + number2 + "=" + answer + ((number1 - number2 == answer) ? " correct" : " wrong");
+
+            }
+            long endTime = System.currentTimeMillis();
+            long testTime = endTime - startTime;
+
+            System.out.println("Correct count is " + correctCount + "\nTest time is " + testTime / 1000 + " seconds\n" + output);
+
+        }
+    }
+}
