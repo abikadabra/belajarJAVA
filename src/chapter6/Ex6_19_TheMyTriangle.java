26cc6bf23eac5e8acc8763a1ccf4eee9f1d79701
abikadabra <abidarda28@gmail.com>
2016-08-12 22:12:19 +0700

    Chapter6 exercise19

---
+++ src/chapter6/Ex6_19_TheMyTriangle.java
@@ -0,0 +1,32 @@
+package chapter6;
+
+/**
+ * Created by USER on 8/11/2016.
+ *
+ *
+ *
+rumus = (Geometry: area of a triangle) Write a program that prompts the user to enter
+three points (x1, y1), (x2, y2), (x3, y3) of a triangle and displays its area.
+The formula for computing the area of a triangle is
+s = (side1 + side2 + side3)/2;
+area = 2s(s - side1)(s - side2)(s - side3)
+
+--------------------------------------------------------------------------------------------
+
+(The MyTriangle class) Create a class named MyTriangle that contains the
+following two methods:
+Return true if the sum of any two sides is
+greater than the third side.
+public static boolean isValid(double side1, double side2, double side3)
+Return the area of the triangle.
+public static double area(double side1, double side2, double side3)
+
+        Write a test program that reads three sides for a triangle and computes the area if
+        the input is valid. Otherwise, it displays that the input is invalid. The formula for
+        computing the area of a triangle is given in Programming Exercise 2.19.
+ */
+
+
+public class Ex6_19_TheMyTriangle {
+
+}
